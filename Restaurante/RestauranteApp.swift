84e80dfbf12431b89207d2d3a27be7fd52b3a6fc
Marcelo <marcelo.neri@iest.edu.mx>
2025-11-07 17:49:52 -0600

    Initial Commit

---
+++ Restaurante/RestauranteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestauranteApp.swift
+//  Restaurante
+//
+//  Created by win603 on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RestauranteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
